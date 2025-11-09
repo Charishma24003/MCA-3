dcbda2a306d22175cf4d6b5516dde7eaa0739875
Charishma24003 <charishmais003@gmail.com>
2025-11-09 10:50:06 +0530

    uploading the 2nd program ie., Validation

---
+++ Angular/validation/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('validation');
+  username = "ngForm";
+}
