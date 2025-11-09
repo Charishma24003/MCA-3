5a2ab4795f669f241a4abda8506154245c34436c
Charishma24003 <charishmais003@gmail.com>
2025-11-09 11:36:09 +0530

    Uploading 3rd program, ie., Date  and other built in pipes

---
+++ Angular/pipeDate/src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('pipeDate');
+  currentDate = new Date();
+  customDate = new Date(2023, 11, 25);
+  birthDate = new Date(24, 10, 2003);
+  userDate: any;
+}
